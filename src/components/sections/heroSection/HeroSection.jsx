b4ca5e40edee27755b9b3cf05cce620f619f8310
mutednew <artehaguk@gmail.com>
2025-07-07 17:02:34 +0300

    create hero section

---
+++ src/components/sections/heroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cl from './HeroSection.module.css';
+
+const HeroSection = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import cl from './Home.module.css';
+import Header from "../../components/header/Header.jsx";
+import HeroSection from "../../components/sections/heroSection/HeroSection.jsx";
 
 const Home = () => {
     return (
-        <div>
-            HOME
-        </div>
+        <>
+            <Header />
+            <HeroSection />
+        </>
     );
 };
 
