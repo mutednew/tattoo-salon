7667325aa393fd3138d613dd084e3ff6ce786a93
Temna Anastasiia <anastasiayatemnaya@gmail.com>
2025-07-07 22:35:29 +0300

    footerV1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
 
             <div className={cl.container}>
                 <CultureSection />
-                <Footer />
             </div>
+            <Footer />
         </>
     );
 };
