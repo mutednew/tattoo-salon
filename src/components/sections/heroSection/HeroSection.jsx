fa0cfa456edac17a4d0d47227789b5367259028e
mutednew <artehaguk@gmail.com>
2025-07-07 19:13:01 +0300

    hero v1

--- src/components/sections/heroSection/HeroSection.jsx
+++ src/components/sections/heroSection/HeroSection.jsx
@@ -3,8 +3,31 @@ import cl from './HeroSection.module.css';
 
 const HeroSection = () => {
     return (
-        <div>
+        <div className={cl.hero}>
+            <div className={cl.container}>
+                {/* Левая часть — изображение */}
+                <div className={cl.left}>
+                    <div className={cl.imageWrapper}>
+                        <img src="/hero_logo.png" alt="Tattoo" />
+                    </div>
 
+                    <h1 className={cl.backgroundText}>TATTOO</h1>
+                </div>
+
+                {/* Правая часть — текст и кнопка */}
+                <div className={cl.right}>
+                    <h2 className={cl.title}>Tattoo Studio</h2>
+                    <p className={cl.text}>
+                        Lorem Ipsom Sole Dowa Bro Js F Asd Kasmd Axsdkakj Askd Asna Ksdka A Skd Asdm Asdj Asn Asjd Asjn Asn Ask
+                    </p>
+
+                    <button className={cl.button}>BOOK NOW!</button>
+                </div>
+            </div>
+
+            <div className={cl.sideText}>
+                <span>FAEZ</span>
+            </div>
         </div>
     );
 };
